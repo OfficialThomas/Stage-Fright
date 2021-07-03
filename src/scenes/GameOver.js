a91a8b1b71e74190119bec3bf3296074d3f6870b
ThomasN <thomasn@ucsc.edu>
2021-07-02 21:34:12 -0700

    navigation between scenes

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -24,10 +24,20 @@ class GameOver extends Phaser.Scene {
         //game over art
 
         //game over text
-        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'Game Over', gameConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 - borderUISize*4, 'Game Over', gameConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 + borderUISize*4, 'Punch (F) to return to the main menu', gameConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 + borderUISize*5, 'Kick (J) to play again', gameConfig).setOrigin(0.5);
         
         //define keys
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
         keyJ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.J);
+    }
+
+    update(){
+        if(Phaser.Input.Keyboard.JustDown(keyF)){
+            this.scene.start('menuScene');
+        } else if(Phaser.Input.Keyboard.JustDown(keyJ)){
+            this.scene.start('playScene');
+        }
     }
 }
