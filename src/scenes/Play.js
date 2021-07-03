a17afca0dab654832b3e94963388831eb6dcac92
ThomasN <thomasn@ucsc.edu>
2021-07-02 19:48:37 -0700

    found positions for the player in the play scene

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -21,7 +21,13 @@ class Play extends Phaser.Scene {
             fixedWidth: 0
         }
         
-        //play art
+        //play art:
+        //player
+        this.add.rectangle(game.config.width/2 - borderPadding*20, game.config.height/2 + borderPadding*4, borderUISize*2, borderUISize*4, 0x0000FF).setOrigin(0.5);
+        //square F
+        this.add.rectangle(game.config.width/2 - borderPadding*13.9, game.config.height/2 + borderPadding*1.05, borderUISize*2, borderUISize*2, 0x00FF00).setOrigin(0.5);
+        //square J
+        this.add.rectangle(game.config.width/2 - borderPadding*13.9, game.config.height/2 + borderPadding*7, borderUISize*2, borderUISize*2, 0xFF0000).setOrigin(0.5);
 
         //play text
         this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'Play Scene', gameConfig).setOrigin(0.5);
