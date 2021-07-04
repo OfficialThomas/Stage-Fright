56aa41db495aedb0dd3808f0525dd50fac961a5b
ThomasN <thomasn@ucsc.edu>
2021-07-04 10:05:51 -0700

    enemy

--- src/prefabs/Enemy.js
+++ src/prefabs/Enemy.js
@@ -1,16 +1,14 @@
 //Enemy prefab
 class Enemy extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y, height, width, texture, frame){
-        super(scene, x, y, height, width, texture, frame);
-
-        //add object to scene
+    constructor(scene, x, y, texture, frame, pointValue) {
+        super(scene, x, y, texture, frame);
         scene.add.existing(this);
         this.points = pointValue;
         this.moveSpeed = 1.75;
     }
 
-    update(){
-        //move the enemy towards the player
+    update() {
+        //move enemy left
         this.x -= this.moveSpeed;
     }
 }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -31,14 +31,10 @@ class Play extends Phaser.Scene {
         //tile sprite
         this.stage = this.add.tileSprite(0, 0, 640, 480, 'stage').setOrigin(0, 0);
         //player
-        this.p1 = new Player(this, game.config.width/2 - borderPadding*20, game.config.height/2, 'player', 0, 3).setOrigin(0.5);
+        this.p1 = new Player(this, game.config.width/2 - borderPadding*20, game.config.height/2, 'player', 0).setOrigin(0.5);
         this.p1.setScale(5);
-        //this.add.rectangle(game.config.width/2 - borderPadding*20, game.config.height/2, borderUISize*2, borderUISize*4, 0x0000FF).setOrigin(0.5);
-        //square F
+        this.e1 = new Enemy(this, game.config.width/2 + borderPadding*20, game.config.height/2 - borderPadding*4, 'hearthalf', 0).setOrigin(0,0);
         
-        //square J
-        
-
         //values
         this.timeVal = 500;
         this.pScore = 0;
@@ -50,8 +46,8 @@ class Play extends Phaser.Scene {
         this.saysScore = this.add.text(borderUISize + borderPadding*4, game.config.height - borderUISize*3, "Score", gameConfig);
         this.scorePlayer = this.add.text(borderUISize + borderPadding*4, game.config.height - borderUISize*2, this.pScore, gameConfig);
         //lives
-        this.lives = this.add.text(game.config.width - borderUISize*8, game.config.height - borderUISize*2, "Lives", gameConfig);
-        
+        this.lives = this.add.text(game.config.width, game.config.height - borderUISize*2, "Lives", gameConfig);
+
         //define keys
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
         keyJ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.J);
@@ -60,25 +56,13 @@ class Play extends Phaser.Scene {
     update(time, delta){
         //scrolling
         this.stage.tilePositionX += 1.5;
+        
+        this.e1.update();
 
-        //update attack
-        this.timing -= delta;
-        if(this.timing < 0){
-            this.timing = 0;
+        //end game
+        if(this.gameOver){
+            this.scene.start('overScene');
         }
-        
-        if(this.timing <= 0){
-            if(Phaser.Input.Keyboard.JustDown(keyF)){
-                this.timing = timeVal;
-                console.log("punch");
-                this.top = this.add.rectangle(game.config.width/2 - borderPadding*13.9, game.config.height/2 - borderPadding*3, borderUISize*2, borderUISize*2, 0x00FF00).setOrigin(0.5);
-            } else if(Phaser.Input.Keyboard.JustDown(keyJ)){
-                this.timing = timeVal;
-                console.log("kick");
-                this.bottom = this.add.rectangle(game.config.width/2 - borderPadding*13.9, game.config.height/2 + borderPadding*3, borderUISize*2, borderUISize*2, 0xFF0000).setOrigin(0.5);
-            }
-        }
-        
     }
     
 }
