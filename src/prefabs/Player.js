f917225ed8ef632a2517fac34ea9b566cd87b55e
Jackie Nguyen <jahunguy@ucsc.edu>
2021-07-04 15:25:18 -0700

    player key inputs

--- src/prefabs/Player.js
+++ src/prefabs/Player.js
@@ -4,6 +4,28 @@ class Player extends Phaser.GameObjects.Sprite {
         super(scene, x, y, texture, frame);
 
         //add object to scene
-        scene.add.existing(this);
+        scene.add.existing(this);           // add object to existing scene    
+        this.isPunching = false;            // track punching status
+        this.isKicking = false;             // track kicking status 
+
+
+    }
+
+    update () {
+        //punch button
+        if (Phaser.Input.Keyboard.JustDown(keyF) && !this.isPunching) {
+            this.isPunching = true;
+        }
+
+        //kick button
+        if (Phaser.Input.Keyboard.JustDown(keyJ) && !this.isKicking) {
+            this.isKicking = true;
+        }
+    }
+
+    //reset player animation to deafult
+    reset () {
+        this.isPunching = false;
+        this.isKicking = false;
     }
 }
