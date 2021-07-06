30d21111e8548141cfa7b0b10c20febe90ec4324
ThomasN <thomasn@ucsc.edu>
2021-07-06 13:59:09 -0700

    added arcade physics to config

--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
+    //reference for arcade physics
+    //https://github.com/nathanaltice/MovementStudies/blob/master/src/main.js
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+            gravity: {
+                x: 0,
+                y: 0
+            }
+        }
+    },
     scene: [ Menu, Play, GameOver ]
 }
 
